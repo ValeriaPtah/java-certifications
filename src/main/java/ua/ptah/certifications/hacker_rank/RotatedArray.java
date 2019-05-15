cad2bd107970ffdd975f5555aa5d95aecda6a915
ValeriePtah <it2ava@gmail.com>
2019-05-16 01:31:25 +0300

    removed redundant serialized names from StockListingResponse, added right rotation to RotatedArray

--- src/main/java/ua/ptah/certifications/hacker_rank/RotatedArray.java
+++ src/main/java/ua/ptah/certifications/hacker_rank/RotatedArray.java
@@ -17,8 +17,6 @@ class RotatedArray {
   static int[] rotateRight(int[] initArray, int rotationIndex) {
     int realRotationIndex = rotationIndex % initArray.length;
     int[] rotated = new int[initArray.length];
-//    System.arraycopy(initArray, 0, rotated, initArray.length - realRotationIndex, realRotationIndex);
-//    System.arraycopy(initArray, realRotationIndex, rotated, 0, initArray.length - realRotationIndex);
     System.arraycopy(initArray, initArray.length - realRotationIndex, rotated, 0, realRotationIndex);
     System.arraycopy(initArray, 0, rotated, realRotationIndex, initArray.length - realRotationIndex);
     return rotated;
